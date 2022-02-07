ce17bc3a97a2540c708446372f0452923f11e386
flojasch <f.jasch@gmx.net>
2022-02-07 08:42:22 +0100

    3DstartP5Server

--- 3DstartP5Server/server.js
+++ 3DstartP5Server/server.js
@@ -1,11 +1,11 @@
 
-let express = require('express');
-let http = require('http');
-let socketIO = require('socket.io');
+var express = require('express');
+var http = require('http');
+var socketIO = require('socket.io');
 
-let app = express();
-let server = http.createServer(app);
-let io = socketIO(server);
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use('/static', express.static(__dirname + '/static'));
 // Routing
@@ -18,23 +18,18 @@ server.listen(5000, function () {
 });
 // http://localhost:5000
 
-let players = {};
+var players = {};
 io.on('connection', (socket)=> {
-  socket.on('new_player', (player)=> {
-    players[socket.id] = player;
+  socket.on('new_player', (playercoords)=> {
+    players[socket.id] = playercoords;
     console.log('user '+  socket.id+' connected');
   });
-  socket.on('new_blaster',(coords)=>{
-    io.sockets.emit('new_blaster',coords);
-  });
-  socket.on('update_player', (player)=> {
-    players[socket.id]=player;
+  
+  socket.on('update_player', (playercoords)=> {
+    players[socket.id]=playercoords;
     
   });
-  socket.on('player_died', (id)=> {
-    io.sockets.emit('player_deleted',id);
-    delete players[id];
-  });
+ 
   socket.on('disconnect', () => {
     console.log('user ' + socket.id + ' disconnected');
     io.sockets.emit('player_deleted',socket.id);
